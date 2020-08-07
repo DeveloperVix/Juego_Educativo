26ac1cc4c58d81ffb1d60b1868f58090a37d4de9
DeveloperVix <vicomisterio@hotmail.com>
2020-08-06 20:03:39 -0500

    Base mini games
    First architecture for the mini games

---
+++ EducationalGame_Math/Assets/Scripts/Base-Game/MiniGame_Elements.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MiniGameState { Playing, Correct, Failed}
+
+public class MiniGame_Elements : MonoBehaviour
+{
+    public MiniGameState minigameState;
+    [TextArea]
+    public string gameInstruction;
+
+    public UnitElementsScriptable curUnit;
+
+    public GameObject[] miniGamePrefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void InitGameElements()
+    {
+        //Initialize the mini game elements, UI, etc
+    }
+
+    public virtual void GenerateGameElements()
+    {
+        //Create all the elements for the mini game
+    }
+}
