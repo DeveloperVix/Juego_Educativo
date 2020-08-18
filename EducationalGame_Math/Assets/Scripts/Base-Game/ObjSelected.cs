8cd6f6d6fbc3ac2044c6ae85a840ca5c02803590
DeveloperVix <vicomisterio@hotmail.com>
2020-08-18 17:19:25 -0500

    Base Mini game Select Obj
    The base for the mini game select object.
    
    The basic and important elements for the UI and the logic for the game is ready.

---
+++ EducationalGame_Math/Assets/Scripts/Base-Game/ObjSelected.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjSelected : MonoBehaviour
+{
+    bool selected = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Tap en :" + gameObject.name);
+        if(!selected)
+        {
+            MiniGame_Elements.Instace.UpdateGameCondition();
+        }
+        else
+        {
+            MiniGame_Elements.Instace.ChangeAnswer();
+        }
+    }
+}
